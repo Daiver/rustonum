7ec00b47a0a85f17e1365ab5f301e041923a411f
daiver <ra22341@ya.ru>
2015-09-29 15:13:16 +0300

    switched from samples to examples dir

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,9 +2,9 @@
 
 use rustonum::MatrixXf;
 
-fn main() {
+fn main()
+{
     let m1 = mat![1.0, 2.0, 3.0; 4.0, 5.0, 6.0];
     let m2 = MatrixXf::consts(2, 3, -5.0);
     println!("{:?}", m1 + m2);
-
 }
