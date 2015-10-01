1b257b9455c78c902e5990fff28fd8e6e5edbd54
daiver <ra22341@ya.ru>
2015-10-01 14:28:26 +0300

    add examples.common.rs

---
+++ examples/common.rs
@@ -0,0 +1,18 @@
+#[macro_use] extern crate rustonum;
+
+use rustonum::MatrixXf;
+
+fn main()
+{
+    let mat1 = MatrixXf::from_vec(
+        vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+        //create row vector from Vec<f32>
+    let mat2 = mat1.reshape(2, 3); //Create matrix 2 x 3
+    let mat3 = MatrixXf::ones(2, 3); //Create matrix 2 x 3 and fill it by ones
+    let mat4 = &mat2 + &(3.0 * mat3); //component-wise summation and multiplication 
+    let mat5 = mat2 * mat4.t(); //matrix multiplication
+    println!("{}", mat5);
+    let mat6 = mat![-30.0, 50.0; -77.0, -100.0]; //Fill matrix in matlab way
+    let mat7 = mat6 + mat5 / 2.0;
+    println!("{}", mat7.sum());
+}
