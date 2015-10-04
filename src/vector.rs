245690f9f4f3ea031108cc02cef033b36b6ff441
daiver <ra22341@ya.ru>
2015-10-04 14:32:21 +0300

    few tests for vector ops added

--- src/vector.rs
+++ src/vector.rs
@@ -1,5 +1,5 @@
 use num::traits::{Float, Num, NumCast};
-use std::ops::{Index, IndexMut, Add, Mul, Div};
+use std::ops::{Index, IndexMut, Add, Sub, Mul, Div};
 
 
 pub trait Vector<N>: Sized
@@ -15,6 +15,15 @@ pub trait Vector<N>: Sized
 {
     //fn zero()
     fn count(&self) -> usize;
+
+    fn sum(&self) -> N
+    {
+        let mut res = N::zero();
+        for i in (0 .. self.count()){
+            res = res + self[i];
+        }
+        res
+    }
 
     fn dot(&self, _rhs: & Self) -> N
     {
@@ -77,6 +86,11 @@ impl<N> Vector3<N>
     pub fn y(&self) -> N {self[1]}
     pub fn z(&self) -> N {self[2]}
 
+    pub fn zero(&self) -> Self
+    {
+        Vector3{values: [N::zero(), N::zero(), N::zero()]}
+    }
+
     pub fn cross(&self, other: Self) -> Self
     {
         Vector3{values:[
@@ -113,3 +127,55 @@ impl<N> IndexMut<usize> for Vector3<N> where N: Float + Copy + NumCast {
 
 pub type Vector3f = Vector3<f32>;
 pub type Vector3d = Vector3<f64>;
+
+
+
+//FIXME should be rewrited by macros
+impl Add for Vector3f {
+    type Output = Vector3f;
+
+    fn add(self, _rhs: Vector3f) -> Vector3f {
+        Vector3f {values: [
+            self.x() + _rhs.x(),
+            self.y() + _rhs.y(),
+            self.z() + _rhs.z(),
+            ]}
+    }
+}
+
+impl Sub for Vector3f {
+    type Output = Vector3f;
+
+    fn sub(self, _rhs: Vector3f) -> Vector3f {
+        Vector3f {values: [
+            self.x() - _rhs.x(),
+            self.y() - _rhs.y(),
+            self.z() - _rhs.z(),
+            ]}
+    }
+}
+
+impl Mul<f32> for Vector3f {
+    type Output = Vector3f;
+    fn mul(self, factor: f32) -> Vector3f
+    {
+        Vector3f {values: [
+            self.x() * factor,
+            self.y() * factor,
+            self.z() * factor
+        ]}
+    }
+}
+
+impl Mul<Vector3f> for f32 {
+    type Output = Vector3f;
+    fn mul(self: f32, vec: Vector3f) -> Vector3f
+    {
+        Vector3f {values: [
+            vec.x() * self,
+            vec.y() * self,
+            vec.z() * self
+        ]}
+    }
+}
+
--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -38,7 +38,6 @@ fn test_vec3_len02()
 }
 
 
-//cross([1, 2, 3], [5, 6, 7]) -4, 8, -4
 #[test]
 fn test_vec3_cross01()
 {
@@ -49,4 +48,24 @@ fn test_vec3_cross01()
     assert!((v3[0] + 4.0).abs() < 0.0001);
     assert!((v3[1] - 8.0).abs() < 0.0001);
     assert!((v3[2] + 4.0).abs() < 0.0001);
+}
+
+
+#[test]
+fn test_vec3_ops01()
+{
+    let v1 = Vector3f{values: [2.5, 3.5, 1.5]};
+    let v2 = Vector3f{values: [5.0, 7.0, 3.0]};
+    let v3 = v2 - 2.0 * v1;
+    assert!(v3.sum().abs() < 0.001);
+}
+
+#[test]
+fn test_vec3_ops02()
+{
+    let v1 = Vector3f{values: [2.0, 3.0, 1.0]};
+    let v2 = Vector3f{values: [5.0, 7.0, 3.0]};
+    let v3 = Vector3f{values: [1.0, 1.0, 1.0]};
+    let v4 = 2.0 * (v1 + v3 * 0.5) - v2;
+    assert!(v4.sum().abs() < 0.001);
 }
