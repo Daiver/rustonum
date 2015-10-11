99e2229f997e083eb4fb77834c63fa59a6a438f9
daiver <ra22341@ya.ru>
2015-10-11 15:51:27 +0300

    remove geometry primitives

--- src/geometry_primitives.rs
+++
@@ -1,9 +0,0 @@
-
-#[derive(Clone, Copy, Debug)]
-pub struct Vector2f {
-    pub x : f32,
-    pub y : f32,
-}
-
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,9 @@ pub use matrix::MatrixXf;
 
 pub use vector::{Vector, GeometryVector, Vector3, Vector3f, Vector2f};
 
-//pub use geometry_primitives::{Vector2f};
-
 #[macro_use]
 pub mod matrix;
 
 #[macro_use]
 pub mod vector;
-
-pub mod geometry_primitives;
 
